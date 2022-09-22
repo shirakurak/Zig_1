a372167c6588b9bd4c0b26f299d551b4daa33304
shirakurak <surakura@hotmail.co.jp>
2022-09-22 22:26:56 +0900

    update

--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
 
 test "basic test" {
