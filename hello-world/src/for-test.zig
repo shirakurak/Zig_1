567738505e193c4a67f9503126fa71353bbfc895
shirakurak <surakura@hotmail.co.jp>
2022-09-23 20:37:20 +0900

    if while for

---
+++ hello-world/src/for-test.zig
@@ -0,0 +1,18 @@
+test "for" {
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
--- hello-world/src/main.zig
+++ hello-world/src/main.zig
@@ -8,3 +8,25 @@ pub fn main() anyerror!void {
 test "basic test" {
     try std.testing.expectEqual(10, 3 + 7);
 }
+
+const expect = @import("std").testing.expect;
+
+test "if statement" {
+    const a = true;
+    var x: u16 = 0;
+    if (a) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    try expect(x == 1);
+}
+
+test "while with continue expression" {
+    var sum: u8 = 0;
+    var i: u8 = 1;
+    while (i <= 10) : (i += 1) {
+        sum += i;
+    }
+    try expect(sum == 55);
+}
