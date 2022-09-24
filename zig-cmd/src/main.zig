22343d40eff3acc6c801feeb1739b08a1c5ac8c2
shirakurak <surakura@hotmail.co.jp>
2022-09-24 20:16:13 +0900

    値と型

--- zig-cmd/src/main.zig
+++ zig-cmd/src/main.zig
@@ -1,9 +1,14 @@
 const std = @import("std");
 const os = std.os;
+const print = std.debug.print;
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-    std.log.info("{s}", .{std.os.argv[1]}); // zig run ./src/main.zig -- 42
+    std.log.info("main() start", .{});
+    
+    const a: [*:0]u8 = std.os.argv[1];
+    print("値:{s}\n型:{s}\n", .{a, @typeName(@TypeOf(std.os.argv[1]))},);
+
+    std.log.info("main() end", .{});
 }
 
 test "basic test" {
