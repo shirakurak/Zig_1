2aae341894cac9da0d9507e1f2409ed99821cb8b
shirakurak <surakura@hotmail.co.jp>
2022-09-24 19:43:15 +0900

    引数を受け取り出力する実装

--- zig-cmd/src/main.zig
+++ zig-cmd/src/main.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
+const os = std.os;
 
 pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
+    std.log.info("{s}", .{std.os.argv[1]}); // zig run ./src/main.zig -- 42
 }
 
 test "basic test" {
