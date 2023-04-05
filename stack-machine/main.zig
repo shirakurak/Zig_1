419189d3ab90600d79335c4fbccf1e15645620d3
shirakurak <ken1shirakura@gmail.com>
2023-04-05 09:13:58 +0900

    call function

--- stack-machine/main.zig
+++ stack-machine/main.zig
@@ -2,4 +2,9 @@ const std = @import("std");
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
+    pop();
+}
+
+fn pop() void {
+    std.debug.print("pop function called\n", .{});
 }
