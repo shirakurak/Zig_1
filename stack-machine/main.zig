81f954500039f3b7309e1edf54c8fd958e2aa70e
shirakurak <ken1shirakura@gmail.com>
2023-04-06 08:29:47 +0900

    add push()

--- stack-machine/main.zig
+++ stack-machine/main.zig
@@ -1,8 +1,21 @@
 const std = @import("std");
+const stackSize: usize = 3;
+var stack: [stackSize]u8 = [_]u8{ 0, 0, 0 };
+var stackTop: usize = 0;
 
 pub fn main() void {
     std.debug.print("Hello, {s}!\n", .{"World"});
     pop();
+    push(10);
+}
+
+fn push(val: u8) void {
+    if (stackTop < stackSize) {
+        stack[stackTop] = val;
+        stackTop += 1;
+    } else {
+        std.debug.print("stack overflow\n", .{});
+    }
 }
 
 fn pop() void {
